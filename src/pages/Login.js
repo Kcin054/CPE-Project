5023350bb2a0980d8fc084cfea02fcaebb5cf808
Kcin054 <nickpatiphat001@gmail.com>
2025-01-16 17:31:44 +0700

    ปรับแต่งหน้า Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Login.css';
-import { Button , Input }from"antd" ;
+import { Button , Input , Row , Col }from"antd" ;
+import { Content, Header } from 'antd/es/layout/layout';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -28,13 +29,42 @@ function Login() {
   return (
     <form className='form-login' id="loginForm"> 
       <div className='container'>
-        <p>Login</p>
-        <Input placeholder="User Name"></Input>
-        <Input placeholder='Password'></Input>
-        <h>--------------------------------------</h>
-        <Button style={{backgroundColor:'green'}}>Sign in</Button>
-        <Button style={{backgroundColor:'blue'}}>Sign up</Button>
-    </div>
+
+        <Header className='header'>Login</Header>
+
+        <Content className='content-login'>
+          
+          <Row className='user'>
+            <Row className='input-user'>
+              <div>User Name</div>
+              <Input placeholder="User Name"></Input>
+            </Row>
+          </Row>
+          
+          <Row className='pass'>
+            <Row className='input-password'>
+              <div>Password</div>
+              <Input placeholder='Password'></Input>
+            </Row>
+          </Row>
+
+          <Row className='button'>
+            <Col style={{marginRight:'2vw'}}>
+              <Button style={{backgroundColor:'white'}}>Sign Up</Button>
+            </Col>
+
+            <Col>
+              <Button style={{backgroundColor:'white'}}>Sign In</Button>
+            </Col>
+          </Row>
+
+          <Row style={{float:"right", marginRight:'2vw'}}>
+            <a>Forget Password?</a>
+          </Row>
+
+        </Content>
+
+      </div>
     </form>
   );
 }
